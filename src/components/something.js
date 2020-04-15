dc66b333e79e98b44c2debc29c00eba05a7063bf
bowtieman143 <steven@onloaddevelopment.com>
2020-04-15 09:55:12 -0700

    This is hopefully going to show the content

---
+++ src/components/something.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Something = props => {
+  return <div>{props.text * 2}</div>
+}
+
+export default Something
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello world</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  const [count, setCount] = useState(0)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello world</h1>
+      <ul>
+        {data.allWordpressWpProduct.edges.map(({ node }, index) => (
+          <li key={index}>
+            <p>{node.title}</p>
+          </li>
+        ))}
+      </ul>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query MyQuery {
+    allWordpressWpProduct {
+      edges {
+        node {
+          id
+          title
+          type
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
