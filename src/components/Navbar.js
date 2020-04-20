7722c8d6cbea05df9dd61d5d744341d3542dd387
bowtieman143 <steven@onloaddevelopment.com>
2020-04-20 13:42:28 -0700

    fixed the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import NavDropdown from "react-bootstrap/NavDropdown"
 import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 
 const MainNavbar = () => (
   <StaticQuery
@@ -19,37 +21,58 @@ const MainNavbar = () => (
       }
     `}
     render={data => (
-      <Navbar className="sticky-top p-0 pb-2" bg="light" expand="lg">
-        <Container className="text-center justify-content-center">
-          <Navbar.Brand href="#home" className="m-0 d-block">
-            <img
-              src={data.file.childImageSharp.sizes.src}
-              alt="help"
-              className="m-0 d-block"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+      <div className="bg-light">
+        <Container>
+          <Row>
+            <Col>
+              <img
+                src={data.file.childImageSharp.sizes.src}
+                alt="help"
+                className="ml-auto mr-auto my-0 d-block"
+              />
+            </Col>
+          </Row>
         </Container>
-      </Navbar>
+        <Navbar
+          className="sticky-top p-0 pb-2 text-dark"
+          bg="light"
+          expand="lg"
+        >
+          <Container className="text-center justify-content-center">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto mr-auto">
+                <NavDropdown
+                  style={{ color: "#000000" }}
+                  title="Shop"
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item href="#action/3.1">Shop</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link style={{ color: "#000000" }} href="#link">
+                  Blog
+                </Nav.Link>
+                <Nav.Link style={{ color: "#000000" }} href="#link">
+                  About Us
+                </Nav.Link>
+                <Nav.Link style={{ color: "#000000" }} href="#link">
+                  Gift Cards
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
     )}
   />
 )
