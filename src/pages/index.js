1d41f9c7b7d7bf8bf3a8d340fc2b4babb06b0541
bowtieman143 <steven@onloaddevelopment.com>
2020-04-24 07:28:25 -0700

    help please

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,37 +12,37 @@ const IndexPage = ({ data }) => {
   useEffect(() => {
     console.log("this is in the console")
 
-    // let deferredPrompt
+    let deferredPrompt
 
     window.addEventListener("beforeinstallprompt", e => {
       console.log("this is firing off")
 
-      // // Prevent the mini-infobar from appearing on mobile
-      // e.preventDefault()
-      // // Stash the event so it can be triggered later.
-      // deferredPrompt = e
-      // // Update UI notify the user they can install the PWA
-      // showInstallPromotion()
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault()
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e
+      // Update UI notify the user they can install the PWA
+      showInstallPromotion()
     })
 
-    //   buttonInstall.addEventListener("click", e => {
-    //     // Hide the app provided install promotion
-    //     hideMyInstallPromotion()
-    //     // Show the install prompt
-    //     deferredPrompt.prompt()
-    //     // Wait for the user to respond to the prompt
-    //     deferredPrompt.userChoice.then(choiceResult => {
-    //       if (choiceResult.outcome === "accepted") {
-    //         console.log("User accepted the install prompt")
-    //       } else {
-    //         console.log("User dismissed the install prompt")
-    //       }
-    //     })
-    //   })
+    buttonInstall.addEventListener("click", e => {
+      // Hide the app provided install promotion
+      hideMyInstallPromotion()
+      // Show the install prompt
+      deferredPrompt.prompt()
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt")
+        } else {
+          console.log("User dismissed the install prompt")
+        }
+      })
+    })
 
-    //   window.addEventListener("appinstalled", evt => {
-    //     console.log("a2hs installed")
-    //   })
+    window.addEventListener("appinstalled", evt => {
+      console.log("a2hs installed")
+    })
   })
   return (
     <Layout>
